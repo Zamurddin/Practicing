6bd3408659e74f43d74c1df33192277b55f1e97f
Zameer Katwal <zameer.katwal@gmail.com>
2021-11-13 10:41:22 +0530

    New Project Exporting to repository

---
+++ src/main/java/com/springboot/firstdemo/SpringBootFirstDemo/SpringBootFirstDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.firstdemo.SpringBootFirstDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstDemoApplication.class, args);
+	}
+
+}
